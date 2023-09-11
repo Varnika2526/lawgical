5441d20fda8f44ee2a0a4215ddde4388a50126ee
Varnika Dayal <varnikadayal60@gmail.com>
2023-09-11 23:07:23 +0530

    first-react-project

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function About(){
+    return(
+        <>
+        <div  id="about" class="f">
+        <h2 class="txt">How does Online Legal India Ensure Customer Satisfaction</h2>
+         <p class="text"> We have created the system of HAPPY CODE to measure customer satisfaction. As part of this, our customers are sent a unique alphanumeric code,
+           consisting of 8 characters. We track customer satisfaction by asking the clients to share their happy codes with our agents 
+           only if they are generally pleased with our services. Since we are a customer-centric company always prioritizing the success of our clients, the HAPPY CODE program helps us to gauge the customers’ happiness and loyalty in our products or services.</p>
+      </div>
+      <br/>
+      <div class="reach">
+        <h2>Talk to us</h2>
+        <br/>
+        <button type="submit">
+          Contact Us Now!
+        </button>
+      </div>
+        </>
+    )
+}
+export default About;
---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from"react";
+import './App.css';
+import Header from "./Header.js";
+import Body from "./Body.js";
+import About from "./About.js";
+
+function App() {
+  return (
+    <>
+    <div>
+      <Header/>
+      <Body/>
+      <About/>
+      
+    </div>
+    </>
+  );
+};
+
+export default App;
